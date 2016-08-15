15e1dadbb24dbb861e1feb781706cd3efae8f39f
Anton Jansson <anton_jansson@live.se>
2016-08-15 11:31:50 +0200

    Fixed problem with assumed order for assembler tests.

--- assembler/tests/assembler-test.h
+++ assembler/tests/assembler-test.h
@@ -8,6 +8,7 @@
 #include <locale>
 #include <cxxtest/TestSuite.h>
 #include <cstdio>
+#include <vector>
 
 //Executes the given command
 std::string executeCmd(const char* cmd) {
@@ -68,33 +69,96 @@ std::string readFile(std::string programName) {
 	return content;
 }
 
+std::vector<std::string> splitString(std::string str, std::string delimiter) {
+	std::vector<std::string> parts;
+
+	std::size_t pos = 0;
+	std::string token;
+	while ((pos = str.find(delimiter)) != std::string::npos) {
+		token = str.substr(0, pos);
+		parts.push_back(token);
+		str.erase(0, pos + delimiter.length());
+	}
+
+	parts.push_back(str);
+	return parts;
+}
+
+//Splits the given string into parts
+std::vector<std::string> splitParts(std::string str) {
+	std::vector<std::string> parts;
+	std::string part;
+	int body = 0;
+
+	for (auto& line : splitString(str, "\n")) {
+		if (line == "{") {
+			body++;
+		} else if (line == "}") {
+			body--;
+		}
+
+		if (line == "" && body == 0) {
+			parts.push_back(part);
+			part.clear();
+		}
+
+		part += line + "\n";
+	}
+
+	parts.push_back(part);
+	return parts;
+}
+
+std::string joinParts(std::vector<std::string> parts) {
+	//Sort parts
+	std::sort(parts.begin(), parts.end());
+
+	//Join
+	std::string str = "";
+
+	bool isFirst = true;
+	for (auto& part : parts) {
+		if (!isFirst) {
+			str += "\n";
+		} else {
+			isFirst = false;
+		}
+
+		str += part;
+	}
+
+	return str;
+}
+
+#define ASM_ASSERT_EQUALS(x, y) TS_ASSERT_EQUALS(joinParts(splitParts(x)), joinParts(splitParts(y)))
+
 class AssemblerTestSuite : public CxxTest::TestSuite {
 public:
 	void testCase1() {
-		TS_ASSERT_EQUALS(invokeAsm("test1"), readFile("test1"));
+		ASM_ASSERT_EQUALS(invokeAsm("test1"), readFile("test1"));
 	}
 
 	void testCase2() {
-		TS_ASSERT_EQUALS(invokeAsm("test2"), readFile("test2"));
+		ASM_ASSERT_EQUALS(invokeAsm("test2"), readFile("test2"));
 	}
 
 	void testCase3() {
-		TS_ASSERT_EQUALS(invokeAsm("test3"), readFile("test3"));
+		ASM_ASSERT_EQUALS(invokeAsm("test3"), readFile("test3"));
 	}
 
 	void testCase4() {
-		TS_ASSERT_EQUALS(invokeAsm("test4"), readFile("test4"));
+		ASM_ASSERT_EQUALS(invokeAsm("test4"), readFile("test4"));
 	}
 
 	void testCase5() {
-		TS_ASSERT_EQUALS(invokeAsm("test5"), "The function 'test(Int)' is already defined.\n");
+		ASM_ASSERT_EQUALS(invokeAsm("test5"), "The function 'test(Int)' is already defined.\n");
 	}
 
 	void testCase6() {
-		TS_ASSERT_EQUALS(invokeAsm("test6"), readFile("test6"));
+		ASM_ASSERT_EQUALS(invokeAsm("test6"), readFile("test6"));
 	}
 
 	void testCase7() {
-		TS_ASSERT_EQUALS(invokeAsm("test6"), readFile("test6"));
+		ASM_ASSERT_EQUALS(invokeAsm("test6"), readFile("test6"));
 	}
 };
